2a94563875e37f41075c7d2abf788ab0c50b9166
Walrus <81767572+jeffMullen@users.noreply.github.com>
2022-09-26 15:41:17 -0400

    adding image to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 import classes from "./Header.module.css";
+import mealImg from "../../assets/images/meals.jpg";
 
 const Header = (props) => {
   return (
-    <header className={classes.header}>
-      <h1>ReactMeals</h1>
+    <header>
+      <div className={classes.header}>
+        <h1>ReactMeals</h1>
+      </div>
+      <div className={classes["main-image"]}>
+        <img src={mealImg} alt="Table of food" />
+      </div>
     </header>
   );
 };
