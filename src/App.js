8631c0f902ccd2fc8667cdbdb44eb61cf83fd2a8
Walrus <81767572+jeffMullen@users.noreply.github.com>
2022-09-26 15:23:00 -0400

    creating header base

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+import Header from "./components/Header/Header";
+
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
